ecdf7dcb9b6ceccbc9f502cc42a7017c7bc457b8
rhondamoananui <rhonda29@vodafone.co.nz>
2015-09-26 16:05:35 +1200

    updated images & portfolio.php

--- ci/application/views/portfolio.php
+++ ci/application/views/portfolio.php
@@ -3,7 +3,7 @@
 	<h1 id="portfolio">Some of my Work</h1>
 	<div class="portfolio-section" >
 		<div>
-			<a href="http://webcoder.co.nz/fttl"><h2>Fly to the Limit</h2><img src="../assets/img/screenshots/fttl-ss1.png" class="screenshots" alt="fly to the limit"></a>
+			<a href="http://webcoder.co.nz/fttl"><h2>Fly to the Limit</h2><img src="../assets/img/screenshots/fttl-ss1.jpg" class="screenshots" alt="fly to the limit"></a>
 			<div>
 				<a class="btn" href="https://github.com/rhondamoananui/webcoder">View on &nbsp;<i class="fa fa-github fa-2x"></i> &nbsp;Github</a>
 				<a class="btn btn-demo" href="http://webcoder.co.nz/fttl">Demo</a>
@@ -11,7 +11,7 @@
 		</div>
 
 		<div>
-			<a href="http://webcoder.co.nz/junksunday"><h2>Junksunday</h2><img src="../assets/img/screenshots/junksunday-ss.png" class="screenshots" alt="junk sunday"></a>
+			<a href="http://webcoder.co.nz/junksunday"><h2>Junksunday</h2><img src="../assets/img/screenshots/junksunday-ss.jpg" class="screenshots" alt="junk sunday"></a>
 			<div>
 				<a class="btn" href="https://github.com/rhondamoananui/webcoder">View on &nbsp;<i class="fa fa-github fa-2x"></i>&nbsp;Github</a>
 				<a class="btn btn-demo" href="http://webcoder.co.nz/junksunday">Demo</a>
@@ -19,7 +19,7 @@
 		</div>
 
 		<div>
-			<a href="http://www.nzfishingclub.com"><h2>NZ Fishing Club</h2><img src="../assets/img/screenshots/nzfc-ss.png" class="screenshots" alt="new zealand fishing club"></a>
+			<a href="http://www.nzfishingclub.com"><h2>NZ Fishing Club</h2><img src="../assets/img/screenshots/nzfc-ss.jpg" class="screenshots" alt="new zealand fishing club"></a>
 			<div>
 				<a class="btn" href="https://github.com/rhondamoananui/NZFC">View on &nbsp;<i class="fa fa-github fa-2x"></i>&nbsp;Github</a>
 				<a class="btn btn-demo" href="http://nzfishingclub.co.nz">Demo</a>
@@ -27,22 +27,22 @@
 		</div>
 
 		<div>
-			<a href="#"><h2>Escort Central</h2><img src="../assets/img/screenshots/ec-ss.png" class="screenshots" alt="escort central"></a>
+			<a href="#"><h2>Awesome Stuff 365</h2><img src="../assets/img/screenshots/as-ss.jpg" class="screenshots" alt="awesome stuff 365"></a>
 			<div>
-				<a class="btn" href="https://github.com/rhondamoananui/EC">View on &nbsp;<i class="fa fa-github fa-2x"></i>&nbsp;Github</a>
-				<a class="btn btn-demo" href="http://escortcentral.com.au">Demo</a>
+				<a class="btn" href="https://github.com/rhondamoananui">View on &nbsp;<i class="fa fa-github fa-2x"></i>&nbsp;Github</a>
+				<a class="btn btn-demo" href="http://awesomestuff365.com">Demo</a>
 			</div>
 		</div>
 
 		<div>
-			<a href="#"><h2>Awesome Things</h2><img src="../assets/img/screenshots/at-ss.png" class="screenshots" alt="awesome things"></a>
+			<a href="#"><h2>Awesome Things</h2><img src="../assets/img/screenshots/at-ss.jpg" class="screenshots" alt="awesome things"></a>
 			<div>
 				<a class="btn" href="#">View on &nbsp;<i class="fa fa-github fa-2x"></i>&nbsp;Github</a>
 				<a class="btn btn-demo" href="#">Demo</a>
 			</div>
 		</div>
 		<div>
-			<a href="#"><h2>Webcoder</h2><img src="../assets/img/screenshots/webcoder-ss.png" class="screenshots" alt="webcoder"></a>
+			<a href="#"><h2>Webcoder</h2><img src="../assets/img/screenshots/webcoder-ss.jpg" class="screenshots" alt="webcoder"></a>
 			<div>
 				<a class="btn" href="https://github.com/rhondamoananui/webcoder">View on &nbsp;<i class="fa fa-github fa-2x"></i>&nbsp;Github</a>
 				<a class="btn btn-demo" href="https://github.com/rhondamoananui/webcoder">Demo</a>
